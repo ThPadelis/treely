20970cae5eea557a098b66b230cbc2d351eb060d
ThPadelis <padelis.theodosiou@gmail.com>
2022-10-05 12:34:43 +0300

    UI Improvements
    - Update the style of the tree view
    - Update the style of the available files in FileSelection component

--- src/components/FileSelection.tsx
+++ src/components/FileSelection.tsx
@@ -20,12 +20,16 @@ export const FileSelection = () => {
         return name === val?.file?.name;
     }
 
+    const hasErrors = (val: UploadableFile) => {
+        return val.errors && val.errors?.length > 0;
+    }
+
     return <>
         {files.length > 0 && <div className="flex flex-wrap gap-2 justify-evenly">
-            {files.map((f, i) => <div key={i} className={`${isSelectedFile(f) ? 'bg-blue-600 text-white' : 'bg-white'} hover:bg-blue-600 hover:text-white text-center p-3 duration-300 hover:cursor-pointer`} onClick={() => setSelectedFile(f)}>
+            {files.map((f, i) => <div key={i} className={`border ${isSelectedFile(f) ? 'bg-blue-600 text-white border-transparent' : 'bg-white  border-slate-300'} hover:bg-blue-600 hover:text-white text-center p-3 duration-300 hover:cursor-pointer shadow-sm ${hasErrors(f) ? 'bg-red-500 text-white hover:bg-red-600 border-transparent' : ''}`} data-tip="hello world" onClick={() => setSelectedFile(f)}>
                 {f.file.name}
             </div>)}
-            <div className="p-3 duration-300 hover:cursor-pointer bg-red-500 hover:bg-red-600 text-white text-center" onClick={removeFiles}>Remove files</div>
+            <div className="p-3 duration-300 hover:cursor-pointer bg-red-500 hover:bg-red-600 text-white text-center shadow-sm" onClick={removeFiles}>Remove files</div>
         </div>}
     </>
 
--- src/components/SingleFile.tsx
+++ src/components/SingleFile.tsx
@@ -7,21 +7,47 @@ interface SingleFileProps {
     file: UploadableFile;
 }
 
+const theme = {
+    scheme: 'tomorrow',
+    author: 'chris kempson (http://chriskempson.com)',
+    base00: '#1d1f21',
+    base01: '#282a2e',
+    base02: '#373b41',
+    base03: '#969896',
+    base04: '#b4b7b4',
+    base05: '#c5c8c6',
+    base06: '#e0e0e0',
+    base07: '#ffffff',
+    base08: '#cc6666',
+    base09: '#de935f',
+    base0A: '#f0c674',
+    base0B: '#b5bd68',
+    base0C: '#8abeb7',
+    base0D: '#81a2be',
+    base0E: '#b294bb',
+    base0F: '#a3685a'
+};
+
+
 export const SingleFile = ({ file }: SingleFileProps) => {
     const [view, setView] = useState<any>();
+    const { file: actFile, errors } = file;
 
     useEffect(() => {
         const reader = new FileReader();
-        reader.readAsText(file.file);
+        reader.readAsText(actFile);
         reader.onload = (event) => {
             parseString(String(reader.result), (error, data) => {
                 setView(data);
             });
         };
-    }, [view, file])
-    return file.errors?.length === 0 ? <div>
-        <JSONTree data={view} labelRenderer={raw => <strong>{raw}</strong>}
-            valueRenderer={raw => <span>{raw}</span>}
-        />
-    </div> : <div>Unable to read file</div>
+    }, [view, file, actFile])
+    return file.errors?.length === 0 ? <div className="bg-white px-3 py-1 border border-dashed border-slate-300">
+        <JSONTree data={view} theme={theme} labelRenderer={raw => <strong>{raw}</strong>} valueRenderer={raw => <span>{raw}</span>} />
+    </div> : <div className="bg-white p-3 border border-dashed border-slate-300 text-center">
+        <p className="text-lg font-semibold">Unable to read file</p>
+        <div className="mt-2">
+            {errors?.map(x => <p>{x.message}</p>)}
+        </div>
+    </div>
 }
